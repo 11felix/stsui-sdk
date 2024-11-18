c4b9a8c5a8dcac46b93d671a577049d965df29fb
Shrom <shrom@alphafi.xyz>
2024-11-19 04:07:40 +0530

    stsui exchange rate function

---
+++ src/common/utils.ts
@@ -0,0 +1,29 @@
+import { getSuiClient, conf, CONF_ENV, LiquidStakingInfo } from "../index.ts";
+import { Decimal } from "decimal.js";
+
+export async function stSuiExchangeRate(): Promise<string> {
+  const lstInfo = await getLstInfo();
+  if (!lstInfo) {
+    console.error("couldnt fetch lst info object");
+    return "0";
+  }
+  const totalSuiSupply = new Decimal(
+    lstInfo.content.fields.storage.fields.total_sui_supply.toString(),
+  );
+  const totalStSuiSupply = new Decimal(
+    lstInfo.content.fields.lst_treasury_cap.fields.total_supply.fields.value.toString(),
+  );
+  return totalSuiSupply.div(totalStSuiSupply).toString();
+}
+
+export async function getLstInfo(): Promise<LiquidStakingInfo | undefined> {
+  const lstInfo = (
+    await getSuiClient().getObject({
+      id: conf[CONF_ENV].LST_INFO,
+      options: {
+        showContent: true,
+      },
+    })
+  ).data as LiquidStakingInfo;
+  return lstInfo;
+}
