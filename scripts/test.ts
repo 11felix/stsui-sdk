90f5e134432c610dfad4ebf794c327c27fd16140
Shrom <shrom@alphafi.xyz>
2024-11-23 00:51:07 +0530

    new testnet ids

--- scripts/test.ts
+++ scripts/test.ts
@@ -18,7 +18,7 @@ import {
 async function createLst() {
   const { address } = getExecStuff();
   const txb = await create_lst(
-    "0xffa973e9be1e88af45cb9145bd05facbf064f8aeb3b196b2876baac20814c4bf",
+    "0xf1d157309bf608a8b3f60dafb4ba98ea4707d683bdb156ad79246f37ca12861c",
     conf[CONF_ENV].STSUI_COIN_TYPE,
     10,
     10,
@@ -37,7 +37,7 @@ async function setValidators() {
       "0xa0920b0776bf13ee51b009c97b85dbf48100ae1510623b9a767450e4a481a1e2",
       "0x9275c6e27c1ce98b08edb3d88e71880520aa114fbf3745d333252f7a47672882",
     ],
-    [90, 10],
+    [50, 50],
   );
   if (txb)
     // dryRunTransactionBlock(txb);
@@ -47,7 +47,7 @@ async function setValidators() {
 
 async function mint() {
   const { address } = getExecStuff();
-  const txb = await mintStsui("5000000000", { address });
+  const txb = await mintStsui("20000000000", { address });
   if (txb)
     // dryRunTransactionBlock(txb);
     executeTransactionBlock(txb);
@@ -56,13 +56,13 @@ async function mint() {
 
 async function redeemstsui() {
   const { address } = getExecStuff();
-  const txb = await redeem("14985000000", { address });
+  const txb = await redeem("9990000000", { address });
   if (txb) {
     // dryRunTransactionBlock(txb);
     executeTransactionBlock(txb);
   }
 }
-// redeemstsui()
+redeemstsui()
 
 async function collectFee() {
   const { address } = getExecStuff();
@@ -82,6 +82,7 @@ async function refreshh() {
   const txb = await refresh();
   if (txb) {
     executeTransactionBlock(txb);
+    // dryRunTransactionBlock(txb);
   }
 }
 // refreshh();
--- src/common/ids.ts
+++ src/common/ids.ts
@@ -4,19 +4,19 @@ export const conf = {
   testing: {
     // General Contants
     STSUI_FIRST_PACKAGE_ID:
-      "0x740b2b8e11111964dde893a263676137d751b16e0b9b23e5d969e11502692323",
+      "0x84c4c3375cff76d0e2314b6f3f55302281d8aa97bfe73a6e4bb0932b0e9fdc7a",
 
     STSUI_LATEST_PACKAGE_ID:
-      "0x740b2b8e11111964dde893a263676137d751b16e0b9b23e5d969e11502692323",
+      "0x84c4c3375cff76d0e2314b6f3f55302281d8aa97bfe73a6e4bb0932b0e9fdc7a",
 
     LST_INFO:
-      "0x0dce538359b83fd7ee08b8712adbe9472ea10f8f9b69a3bce5dbb197431ec2a8",
+      "0xf778816390c7b22b88d413f84e7e2504ebefb499ccf21a55bae465796de7ff74",
 
     ADMIN_CAP:
-      "0x7c84dd5143438ab97560982829fc3d5eb7f5b9b5840d8e35b1a0300eb8054257",
+      "0xb3e862776559c7875d96758feb9f2e0a1288b28b0db02c8a3b553531ab4702ac",
 
     COLLECTION_FEE_CAP_ID:
-      "0xc0a0dd59fe6517ce2773f199cddc5c25657fdc7f7e5ade95d0cb820d3f51aedb",
+      "0x5e23df49a12aebc237cc8a298f045e917b1acdc50910e72007b0a48e353b1602",
 
     SUI_SYSTEM_STATE_OBJECT_ID: "0x5",
 
@@ -27,7 +27,7 @@ export const conf = {
     SUI_COIN_TYPE: "0x2::sui::SUI",
 
     STSUI_COIN_TYPE:
-      "0x740b2b8e11111964dde893a263676137d751b16e0b9b23e5d969e11502692323::st_sui::ST_SUI",
+      "0xe209c8c545649b6ef73bc35faf34bedaeb0cc7918063d4705d3ad91ff11e4645::st_sui::ST_SUI",
   },
 
   production: {
