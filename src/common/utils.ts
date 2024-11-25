21b009e8f617f4e5534ab7d706bc8de1bbe95cae
Shrom <shrom@alphafi.xyz>
2024-11-25 22:23:37 +0530

    corrected exchange rate function

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -9,7 +9,7 @@ export async function stSuiExchangeRate(): Promise<string> {
   }
   const totalSuiSupply = new Decimal(
     lstInfo.content.fields.storage.fields.total_sui_supply.toString(),
-  );
+  ).minus(new Decimal(lstInfo.content.fields.accrued_spread_fees));
   const totalStSuiSupply = new Decimal(
     lstInfo.content.fields.lst_treasury_cap.fields.total_supply.fields.value.toString(),
   );
