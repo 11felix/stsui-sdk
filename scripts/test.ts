3eb0c71c9bacbe9c9d4ec6194d28c88952d94302
Shrom <shrom@alphafi.xyz>
2024-12-10 21:34:32 +0530

    stsui apr apy

--- scripts/test.ts
+++ scripts/test.ts
@@ -10,6 +10,9 @@ import {
   updateFees,
   getFees,
   FeeConfig,
+  Events,
+  fetchStSuiAPR,
+  fetchStSuiAPY,
 } from "../src/index.ts";
 import {
   dryRunTransactionBlock,
@@ -78,9 +81,9 @@ async function collectFee() {
 
 async function xrate() {
   console.log(await stSuiExchangeRate());
-  console.log((await getFees() as FeeConfig));
+  console.log((await getFees()) as FeeConfig);
 }
-xrate();
+// xrate();
 
 async function refreshh() {
   const txb = await refresh();
@@ -99,3 +102,21 @@ async function update_fee() {
   }
 }
 // update_fee();
+
+async function epochEvents() {
+  let ok = await Events.getEpochChangeEvents({
+    startTime: 1733776365964,
+    endTime: 1733827977386,
+  });
+  console.log(ok);
+}
+// epochEvents();
+
+async function apr() {
+  console.log(await fetchStSuiAPR(2));
+}
+// apr();
+async function apy() {
+  console.log(await fetchStSuiAPY(2));
+}
+apy();
---
+++ src/common/events.ts
@@ -0,0 +1,88 @@
+import { EventId, PaginatedEvents } from "@mysten/sui/client";
+import { getSuiClient } from "./client.js";
+import { getConf } from "./ids.js";
+import { CommonEventParams, EpochChangedEvent, MintEvent } from "./types.js";
+
+export class Events {
+  static async getEpochChangeEvents(
+    params: CommonEventParams,
+  ): Promise<EpochChangedEvent[]> {
+    const events: EpochChangedEvent[] = [];
+    let startTime = 0;
+    let endTime = Date.now();
+    if (params.startTime) {
+      startTime = params.startTime;
+    }
+    if (params.endTime) {
+      endTime = params.endTime;
+    }
+    if (startTime > endTime) {
+      return events;
+    }
+    let hasNext = true;
+    let startCursor: EventId | null | undefined;
+    while (hasNext) {
+      const eventData: PaginatedEvents = await getSuiClient().queryEvents({
+        cursor: startCursor,
+        order: "descending",
+        query: {
+          MoveEventType: `${getConf().STSUI_FIRST_PACKAGE_ID}::events::Event<${getConf().STSUI_FIRST_PACKAGE_ID}::liquid_staking::EpochChangedEvent>`,
+        },
+      });
+
+      for (const eve of eventData.data) {
+        if (Number(eve.timestampMs!) > endTime) {
+          continue;
+        }
+        if (Number(eve.timestampMs!) < startTime) {
+          return events;
+        }
+        const event = eve.parsedJson as EpochChangedEvent;
+        events.push(event);
+      }
+      hasNext = eventData.hasNextPage;
+      startCursor = eventData.nextCursor;
+    }
+    return events;
+  }
+
+  static async getMintEvents(params: CommonEventParams): Promise<MintEvent[]> {
+    const events: MintEvent[] = [];
+    let startTime = 0;
+    let endTime = Date.now();
+    if (params.startTime) {
+      startTime = params.startTime;
+    }
+    if (params.endTime) {
+      endTime = params.endTime;
+    }
+    if (startTime > endTime) {
+      return events;
+    }
+    let hasNext = true;
+    let startCursor: EventId | null | undefined;
+    while (hasNext) {
+      const eventData: PaginatedEvents = await getSuiClient().queryEvents({
+        cursor: startCursor,
+        order: "descending",
+        query: {
+          MoveEventType: `${getConf().STSUI_FIRST_PACKAGE_ID}::events::Event<${getConf().STSUI_FIRST_PACKAGE_ID}::liquid_staking::MintEvent>`,
+        },
+      });
+
+      for (const eve of eventData.data) {
+        if (Number(eve.timestampMs!) > endTime) {
+          continue;
+        }
+        if (Number(eve.timestampMs!) < startTime) {
+          return events;
+        }
+        const event = eve.parsedJson as MintEvent;
+        events.push(event);
+      }
+      hasNext = eventData.hasNextPage;
+      startCursor = eventData.nextCursor;
+    }
+    return events;
+  }
+}
